1bd3c18ab267e0e9ce46f944870549d76e004133
rc_285 <rajonna.choudhury@gmail.com>
2026-06-04 22:41:20 +0530

    add project-1 files

---
+++ project-1/initial_analysis.sql
@@ -0,0 +1,33 @@
+select * from walmart_sales ws;
+
+--"Date" column is character varying data type i.e. its in text/string--
+select column_name,data_type from information_schema.columns where table_name='walmart_sales';
+
+-- output=Friday
+select "Date",to_char(to_date("Date",'DD-MM-YYY'),'Day') as "Day" from walmart_sales ws where "Date"='01-07-2010';
+
+--highest weekly_sale on 24th December 2010 i.e. Christmas Eve
+select "Weekly_Sales","Date","Fuel_Price","Unemployment" from walmart_sales ws order by "Weekly_Sales" desc limit 1;
+
+-- lowest weekly_sale on 3rd December 2010
+select "Weekly_Sales","Date","Fuel_Price","Unemployment" from walmart_sales ws order by "Weekly_Sales" asc limit 1;
+
+--Distinct Year
+select distinct to_char(to_date("Date",'DD-MM-YYYY'),'YYYY') as "Year"
+from walmart_sales ws; --Output: 2010,2011,2012
+
+-- Monthly Sales of 2010
+select to_char(to_date("Date",'DD-MM-YYY'),'Month') as "Month", sum("Weekly_Sales") as "Monthly_Sales" from walmart_sales ws 
+where to_char(to_date("Date",'DD-MM-YYYY'),'YYYY')='2010' group by "Month" order by "Monthly_Sales" desc; 
+-- Highest = December , Lowest = September --
+
+--Monthly Sales of 2011
+select to_char(to_date("Date",'DD-MM-YYY'),'Month') as "Month", sum("Weekly_Sales") as "Monthly_Sales" from walmart_sales ws 
+where to_char(to_date("Date",'DD-MM-YYYY'),'YYYY')='2011' group by "Month" order by "Monthly_Sales" desc;
+-- Highest = December , Lowest = January --
+
+--Monthly Sales of 2012[till October]
+select to_char(to_date("Date",'DD-MM-YYYY'),'Month') as "Month", sum("Weekly_Sales") as "Monthly_Sales" from walmart_sales ws 
+where to_char(to_date("Date",'DD-MM-YYYY'),'YYYY')='2012' group by "Month" order by "Monthly_Sales" desc;
+-- Highest = June , Lowest = January --
+
